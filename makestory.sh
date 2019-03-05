06c4c2b41b20d19bd02b7aedfe6831b9a811d7e0
David Blackman <david@blackmad.com>
2019-03-05 09:18:33 -0500

    ikea colors

--- makestory.sh
+++ makestory.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cp  ../Twine/Stories/talya2.html .
-./tweego-mac-x64  -f sugarcube-3 -o index.html talya2.html audioonend.js  && open -a Google\ Chrome index.html
+./tweego-mac-x64  -f sugarcube-3 -o index.html talya2.html *.js test.css  && open -a Google\ Chrome index.html
