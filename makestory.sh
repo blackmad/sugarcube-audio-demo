3bbd4582eda63c8cd472287969e0d92401b66736
David Blackman <david@blackmad.com>
2019-03-09 14:03:51 -0500

    clean up directories, switch to mp3s

--- makestory.sh
+++ makestory.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cp  ../Twine/Stories/talya2.html .
-./tweego-mac-x64  -f sugarcube-3 -o index.html talya2.html *.js test.css  && open -a Google\ Chrome index.html
+./tweego/tweego-mac-x64  -f sugarcube-3 -o output/index.html talya2.html js/*.js js/*/*.js css/test.css  && open -a Google\ Chrome output/index.html
