04e430b6153fe552740f20442525b53473c08466
David Blackman <david@blackmad.com>
2019-03-10 15:42:36 -0400

    THE END

--- makestory.sh
+++ makestory.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cp ~/Dropbox/Narrative\ Experiments/Twine/Stories/talya2.html .
 # direct compile
 # ./tweego/tweego-mac-x64  -f sugarcube-3 -o output/index.html talya2.html js/*.js js/*/*.js css/test.css  && open -a Google\ Chrome output/index.html
 
